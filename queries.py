b37062e6d915635e4c32aedc3db12bc866d95d60
Viktor Salimov <viktorsalimov544@gmail.com>
2023-01-03 17:23:40 +0300

    Добавил запрос данных пользователя в БД

--- queries.py
+++ queries.py
@@ -3,7 +3,7 @@ from db import db_session
 
 
 def get_user_data(name: str) -> str:
-    # получаем данные пользователя (переменная name) из базы данных
+    # получаем данные пользователя (переменная name) из БД
     user_data = db_session.query(
         Users.login,
         Users.password
