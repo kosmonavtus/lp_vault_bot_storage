16d22399f52d1a9c0df2e1f35fee0a33a2d942a1
black <no_email@email.com>
2023-01-06 17:06:22 +0400

    fix pep8

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ import toml
 from app.user.views import blueprint as user_blueprint
 from app.repositories.views import blueprint as repo_blueprint
 
-def create_app():
+
+def create_app() -> None:
+    """Запускаем приложение из функции."""
     app = Flask(__name__)
-    app.config.from_file("./config.toml", load=toml.load)
+    app.config.from_file('./config.toml', load=toml.load)
     #
     # Тут должен быть инит базы.
     # Не понял как инитнуть базу из апп.
